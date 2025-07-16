7c542aba0cf57b1fec4ed61aebda7a9305332c2e
Roel Leal <angel@kavaup.io>
2025-07-16 17:55:34 -0600

    chore: it was created the workflow for creating the APK for the testing application

--- appambit-testapp/build.gradle.kts
+++ appambit-testapp/build.gradle.kts
@@ -20,8 +20,18 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("appambit.keystore")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
