ebd812f78153a8759c7037a48defe131687c0cef
Roel Leal <angel@kavaup.io>
2025-07-16 18:28:55 -0600

    chore: changed keystore route to will be found in compilation

--- appambit-testapp/build.gradle.kts
+++ appambit-testapp/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("appambit.keystore")
+            storeFile = file("../appambit.keystore")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD")
             keyAlias = System.getenv("SIGNING_KEY_ALIAS")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
