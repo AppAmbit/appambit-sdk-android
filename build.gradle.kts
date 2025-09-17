fd346f0efcbfb76550715ee71366b8d5ef316dce
Roel Leal <angel@kavaup.io>
2025-09-17 13:45:13 -0600

    chore: added extra steps and changing version to 0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    id ("io.github.gradle-nexus.publish-plugin") version ("1.1.0")
+    id ("io.github.gradle-nexus.publish-plugin") version ("1.3.0")
 }
 
 nexusPublishing {
