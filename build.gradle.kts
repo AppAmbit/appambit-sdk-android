570ec9826db09d9a9751b879b56019dd389b7eab
Roel Leal <angel@kavaup.io>
2025-09-16 22:39:06 -0600

    chore: changed pipeline workflow and using nexus to publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,17 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    id ("io.github.gradle-nexus.publish-plugin") version ("1.1.0")
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username = System.getenv("OSSRH_USERNAME")
+            password = System.getenv("OSSRH_PASSWORD")
+
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
+    }
 }
